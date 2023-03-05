b35686bfaf61081c2f3bfe718330041e5ff5c421
Alex Janse <github@alexjanse.nl>
2023-03-05 15:44:51 +0100

    Added circle

--- Math.cs
+++ Math.cs
@@ -16,6 +16,7 @@ namespace LSPainter
     public struct Vector
     {
         public float X, Y;
+        public float Length => (float)Math.Sqrt(X * X + Y * Y);
 
         public Vector(float x, float y)
         {
@@ -25,18 +26,17 @@ namespace LSPainter
 
         public static implicit operator Point(Vector v) => new Point(v.X, v.Y);
 
-        public float Length => (float)Math.Sqrt(X * X + Y * Y);
-
         public static Vector operator +(Vector v) => v;
         public static Vector operator -(Vector v) => new Vector(-v.X, -v.Y);
         public static Vector operator +(Vector u, Vector v) => new Vector(u.X + v.X, u.Y + v.Y);
         public static Vector operator -(Vector u, Vector v) => new Vector(u.X - v.X, u.Y - v.Y);
         public static Vector operator *(float f, Vector v) => new Vector(f * v.X, f * v.Y);
         public static Vector operator *(Vector v, float f) => f * v;
-
         public static float Dot(Vector u, Vector v) => u.X * v.X + u.Y * v.Y;
-
         public static float Determinant(Vector u, Vector v, Vector w) => (w.X - u.X) * (v.Y - u.Y) - (w.Y - u.Y) * (v.X - u.X);
+
+        public static Vector UnitX = new Vector(1, 0);
+        public static Vector UnitY = new Vector(0, 1);
     }
 
     public struct Rectangle
---
+++ Shape/Circle.cs
@@ -0,0 +1,23 @@
+namespace LSPainter.Shapes
+{
+    public class Circle : Shape
+    {
+        public Vector Origin { get; }
+        public float Radius { get; }
+
+        public Circle(Vector origin, float radius)
+        {
+            Origin = origin;
+            Radius = radius;
+
+            Area = (float)Math.PI * Radius * Radius;
+
+            BoundingBox = new Rectangle(Origin.X - Radius, Origin.Y - Radius, Radius * 2, Radius * 2);
+        }
+
+        public override bool IsInside(Vector p)
+        {
+            return (p - Origin).Length <= Radius;
+        }
+    }
+}
