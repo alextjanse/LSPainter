1ec97a59ebe677ea3a1a6a1d62a3d5342d437ba1
Alex Janse <github@alexjanse.nl>
2023-05-02 15:11:28 +0200

    [WIP]

--- ShapePainter/ShapePainterSolution.cs
+++ ShapePainter/ShapePainterSolution.cs
@@ -2,15 +2,15 @@ using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using LSPainter.Maths.Shapes;
 using LSPainter.Maths;
-using LSPainter.LSSolver;
+using LSPainter.LSSolver.CanvasSolution;
 
 namespace LSPainter.ShapePainter
 {
-    public class ShapePainterSolution : CanvasSolution<ShapePainterChange>
+    public class ShapePainterSolution : CanvasSolution
     {
         public ShapePainterSolution(int width, int height) : base(width, height)
         {
-            Score = 0;
+            
         }
     }
 }
