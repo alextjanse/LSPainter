07b7de814762d4cc2b56c4f932c67df6493b8a4f
alextjanse <github@alexjanse.nl>
2023-10-29 22:13:33 +0100

    pretty print

--- src/Program.cs
+++ src/Program.cs
@@ -3,6 +3,7 @@ using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 using OpenTK.Windowing.GraphicsLibraryFramework;
 using LSPainter.DCEL;
+using System.Runtime.Versioning;
 
 namespace LSPainter
 {
@@ -10,6 +11,8 @@ namespace LSPainter
     {
         private static void Main()
         {
+            PrettyPrint();
+
             string file_path = "Images/mona_lisa.jpeg";
 
             ImageHandler image = ImageHandler.FromFile("Mona Lisa", file_path);
@@ -19,6 +22,24 @@ namespace LSPainter
             using (WindowManager window = new WindowManager(windowLayout, image))
             {
                 window.Run();
+            }
+        }
+
+        private static void PrettyPrint()
+        {
+            string[] ascii_art = new[]
+            {
+                @"   __  __             _       _            ",
+                @"  / / / _\_ __   __ _(_)_ __ | |_ ___ _ __ ",
+                @" / /  \ \| '_ \ / _` | | '_ \| __/ _ \ '__|",
+                @"/ /____\ \ |_) | (_| | | | | | ||  __/ |   ",
+                @"\____/\__/ .__/ \__,_|_|_| |_|\__\___|_|   ",
+                @"         |_|                               ",
+            };
+
+            foreach (string line in ascii_art)
+            {
+                Console.WriteLine(line);
             }
         }
     }
